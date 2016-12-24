b04c01a94974a6519704a4e58855f5472902db23
JanCas <jan.l.cas@outlook.com>
2016-12-24 21:00:07 +0100

    supressed all the warnings

--- src/Hamming.java
+++ src/Hamming.java
@@ -488,7 +488,6 @@ public class Hamming {
 	 * in case the file does not exists it prints the Strack Trace
 	 */
 	public void ReceiveMsg(int party) {
-		int y = MaxNumberInCsv(party);
 		String pty = PartytoString(party);
 		ArrayList<Integer> M = new ArrayList<Integer>();
 		System.out.println();
