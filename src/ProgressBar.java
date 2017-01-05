9daf603a2c7221ca09991832236ad33f276b8bea
JanCas <jan.l.cas@outlook.com>
2017-01-04 21:35:57 -0800

    ok

--- src/ProgressBar.java
+++ src/ProgressBar.java
@@ -3,20 +3,25 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
 
+@SuppressWarnings("serial")
 public class ProgressBar extends JPanel{
 	
 	
 	static final int MIN = 0;
 	static final int MAX = 100;
 	JProgressBar pbar = new JProgressBar();
+	JFrame frame = new JFrame();
 	
 	public ProgressBar(){
 		
 		pbar.setMinimum(MIN);
 		pbar.setMaximum(MAX);
 		add(pbar);
-		
-
+		frame.setSize(800, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(pbar);
+		frame.setLocation(400, 400);
+		frame.setVisible(true);
 	}
 	public void display(int d){
 		pbar.setStringPainted(true);
